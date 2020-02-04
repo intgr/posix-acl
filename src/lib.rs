c4eeaca3efce1856fd996edd2d50c68639fcc1f1
Marti Raudsepp <marti@juffo.org>
2020-02-04 20:39:39 +0200

    Add some "real" tests :)

--- src/lib.rs
+++ src/lib.rs
@@ -156,14 +156,65 @@ impl PosixACL {
     }
 }
 
-// TODO write some real tests :)
 #[cfg(test)]
 mod tests {
     use crate::PosixACL;
+    use crate::Qualifier::*;
+    use acl_sys::{ACL_EXECUTE, ACL_READ, ACL_WRITE};
 
     #[test]
     fn new() {
         let acl = PosixACL::new();
         assert_eq!(acl.as_text(), "");
+    }
+    #[test]
+    fn empty_mask() {
+        let mut acl = PosixACL::new();
+        // UserObj and Other qualifiers do not affect mask.
+        acl.set(UserObj, ACL_READ | ACL_WRITE);
+        acl.set(Other, ACL_READ);
+        acl.fix_mask();
+        assert_eq!(acl.as_text(), "user::rw-\nmask::---\nother::r--\n");
+    }
+    #[test]
+    fn other_mask() {
+        let mut acl = PosixACL::new();
+        // GroupObj, User, Group qualifiers affect mask.
+        acl.set(GroupObj, ACL_READ);
+        acl.set(User(0), ACL_WRITE);
+        acl.set(Group(0), ACL_EXECUTE);
+        acl.fix_mask();
+        assert_eq!(
+            acl.as_text(),
+            "user:root:-w-\ngroup::r--\ngroup:root:--x\nmask::rwx\n"
+        );
+    }
+    #[test]
+    fn validate_empty() {
+        let mut acl = PosixACL::new();
+        assert_eq!(acl.validate().unwrap_err().as_str(), "Invalid ACL: ");
+        acl.fix_mask();
+        assert_eq!(
+            acl.validate().unwrap_err().as_str(),
+            "Invalid ACL: mask::---,"
+        );
+    }
+    #[test]
+    fn validate_ok() {
+        let mut acl = PosixACL::new();
+        acl.set(UserObj, ACL_READ | ACL_WRITE);
+        acl.set(GroupObj, ACL_READ | ACL_WRITE);
+        acl.set(Other, 0);
+        assert_eq!(acl.validate(), Ok(()));
+
+        acl.set(User(0), ACL_READ);
+        acl.set(Group(0), ACL_READ);
+        assert_eq!(
+            acl.validate().unwrap_err().as_str(),
+            "Invalid ACL: user::rw-,user:root:r--,group::rw-,group:root:r--,other::---,"
+        );
+
+        acl.fix_mask();
+        assert!(acl.validate().is_ok());
     }
 }
