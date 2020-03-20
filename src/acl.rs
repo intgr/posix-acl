a6c286dd82e5803983dee6aa49fabe243769273a
Marti Raudsepp <marti@juffo.org>
2020-03-21 00:46:38 +0200

    Reorganize, split into multiple modules (#35)

---
+++ src/acl.rs
@@ -0,0 +1,312 @@
+use std::io::Error;
+
+use std::path::Path;
+use std::slice::from_raw_parts;
+use std::str::from_utf8;
+
+use libc::ssize_t;
+use simple_error::SimpleError;
+
+use crate::iter::RawACLIterator;
+use crate::util::{check_pointer, check_return, path_to_cstring, type_description, AutoPtr};
+use crate::Qualifier::*;
+use crate::{ACLEntry, Qualifier, ACL_RWX};
+use acl_sys::{
+    acl_add_perm, acl_calc_mask, acl_clear_perms, acl_create_entry, acl_delete_entry, acl_entry_t,
+    acl_get_file, acl_get_permset, acl_init, acl_permset_t, acl_set_file, acl_set_permset,
+    acl_set_qualifier, acl_set_tag_type, acl_t, acl_to_text, acl_type_t, acl_valid,
+    ACL_TYPE_ACCESS, ACL_TYPE_DEFAULT,
+};
+use std::os::raw::c_void;
+use std::ptr::null_mut;
+use std::{fmt, mem};
+
+/// The ACL of a file.
+pub struct PosixACL {
+    pub(crate) acl: acl_t,
+}
+
+/// Custom debug formatting, since output `PosixACL { acl: 0x7fd74c000ca8 }` is not very helpful.
+impl fmt::Debug for PosixACL {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Not really a tuple, but tuple formatting is compact.
+        fmt.debug_tuple("PosixACL")
+            .field(&self.compact_text())
+            .finish()
+    }
+}
+
+impl Drop for PosixACL {
+    fn drop(&mut self) {
+        AutoPtr(self.acl);
+    }
+}
+
+impl PartialEq for PosixACL {
+    fn eq(&self, other: &Self) -> bool {
+        self.entries() == other.entries()
+    }
+}
+
+impl PosixACL {
+    /// Convert a file mode ("chmod" number) into an ACL. This is the primary constructor.
+    /// Note that modes are usually expressed in Octal, e.g. `PosixACL::new(0o644)`
+    ///
+    /// This creates the minimal required entries. By the POSIX ACL spec, every valid ACL must
+    /// contain at least four entries: UserObj, GroupObj, Mask and Other.
+    ///
+    /// Bits higher than 9 (e.g. SUID flag, etc) are ignored.
+    ///
+    /// ```
+    /// use posix_acl::PosixACL;
+    /// assert_eq!(
+    ///     PosixACL::new(0o751).as_text(),
+    ///     "user::rwx\ngroup::r-x\nmask::r-x\nother::--x\n"
+    /// );
+    /// ```
+    pub fn new(file_mode: u32) -> PosixACL {
+        let mut acl = PosixACL::empty();
+        acl.set(UserObj, (file_mode >> 6) & ACL_RWX);
+        acl.set(GroupObj, (file_mode >> 3) & ACL_RWX);
+        acl.set(Mask, (file_mode >> 3) & ACL_RWX);
+        acl.set(Other, file_mode & ACL_RWX);
+        acl
+    }
+
+    /// Create an empty ACL. NB! Empty ACLs are NOT considered valid.
+    pub fn empty() -> PosixACL {
+        PosixACL::with_capacity(6)
+    }
+
+    /// Create an empty ACL with capacity. NB! Empty ACLs are NOT considered valid.
+    pub fn with_capacity(capacity: usize) -> PosixACL {
+        let acl = unsafe { acl_init(capacity as i32) };
+        check_pointer(acl, "acl_init");
+        PosixACL { acl }
+    }
+
+    /// Read a path's access ACL and return as `PosixACL` object.
+    /// ```
+    /// use posix_acl::PosixACL;
+    /// let acl = PosixACL::read_acl("/etc/motd").unwrap();
+    /// ```
+    pub fn read_acl<P: AsRef<Path>>(path: P) -> Result<PosixACL, SimpleError> {
+        Self::read_acl_flags(path.as_ref(), ACL_TYPE_ACCESS)
+    }
+
+    /// Read a directory's default ACL and return as `PosixACL` object.
+    /// This will fail if `path` is not a directory.
+    ///
+    /// Default ACL determines permissions for new files and subdirectories created in the
+    /// directory.
+    /// ```
+    /// use posix_acl::PosixACL;
+    /// let acl = PosixACL::read_default_acl("/tmp").unwrap();
+    /// ```
+    pub fn read_default_acl<P: AsRef<Path>>(path: P) -> Result<PosixACL, SimpleError> {
+        Self::read_acl_flags(path.as_ref(), ACL_TYPE_DEFAULT)
+    }
+
+    fn read_acl_flags(path: &Path, flags: acl_type_t) -> Result<PosixACL, SimpleError> {
+        let c_path = path_to_cstring(path);
+        let acl: acl_t = unsafe { acl_get_file(c_path.as_ptr(), flags) };
+        if acl.is_null() {
+            bail!(
+                "Error reading {} {}: {}",
+                path.display(),
+                type_description(flags),
+                Error::last_os_error()
+            );
+        }
+        Ok(PosixACL { acl })
+    }
+
+    /// Validate and write this ACL to a path's access ACL. Overwrites any existing access ACL.
+    ///
+    /// Note: this function takes mutable `self` because it automatically re-calculates the magic
+    /// `Mask` entry.
+    pub fn write_acl<P: AsRef<Path>>(&mut self, path: P) -> Result<(), SimpleError> {
+        self.write_acl_flags(path.as_ref(), ACL_TYPE_ACCESS)
+    }
+
+    /// Validate and write this ACL to a directory's default ACL. Overwrites existing default ACL.
+    /// This will fail if `path` is not a directory.
+    ///
+    /// Default ACL determines permissions for new files and subdirectories created in the
+    /// directory.
+    ///
+    /// Note: this function takes mutable `self` because it automatically re-calculates the magic
+    /// `Mask` entry.
+    pub fn write_default_acl<P: AsRef<Path>>(&mut self, path: P) -> Result<(), SimpleError> {
+        self.write_acl_flags(path.as_ref(), ACL_TYPE_DEFAULT)
+    }
+
+    fn write_acl_flags(&mut self, path: &Path, flags: acl_type_t) -> Result<(), SimpleError> {
+        let c_path = path_to_cstring(path);
+        self.fix_mask();
+        self.validate()?;
+        let ret = unsafe { acl_set_file(c_path.as_ptr(), flags, self.acl) };
+        if ret != 0 {
+            bail!(
+                "Error writing {} {}: {}",
+                path.display(),
+                type_description(flags),
+                Error::last_os_error()
+            );
+        }
+        Ok(())
+    }
+
+    /// Iterator of `acl_entry_t`, unsafe
+    pub(crate) unsafe fn raw_iter(&self) -> RawACLIterator {
+        RawACLIterator::new(&self)
+    }
+
+    /// Get all ACLEntry items. The POSIX ACL C API does not allow multiple parallel iterators so we
+    /// return a materialized vector just to be safe.
+    pub fn entries(&self) -> Vec<ACLEntry> {
+        unsafe { self.raw_iter() }
+            .map(ACLEntry::from_entry)
+            .collect()
+    }
+
+    /// Set the permission of `qual` to `perm`. If this `qual` already exists, it is updated,
+    /// otherwise a new one is added.
+    ///
+    /// `perm` must be a combination of the `ACL_` constants, combined by binary OR.
+    pub fn set(&mut self, qual: Qualifier, perm: u32) {
+        let entry = match self.raw_get_entry(&qual) {
+            Some(v) => v,
+            None => self.raw_add_entry(&qual),
+        };
+
+        Self::raw_set_permset(entry, perm);
+    }
+
+    /// Get the current `perm` value of `qual`, if any.
+    pub fn get(&self, qual: Qualifier) -> Option<u32> {
+        let entry = self.raw_get_entry(&qual)?;
+
+        // XXX inefficient, no need to construct ACLEntry.
+        Some(ACLEntry::from_entry(entry).perm)
+    }
+
+    /// Remove entry with matching `qual`. If found, returns the matching `perm`, otherwise `None`
+    pub fn remove(&self, qual: Qualifier) -> Option<u32> {
+        let entry = self.raw_get_entry(&qual)?;
+        let wrapped = ACLEntry::from_entry(entry);
+
+        unsafe {
+            acl_delete_entry(self.acl, entry);
+        }
+
+        // XXX inefficient, no need to construct ACLEntry.
+        Some(wrapped.perm)
+    }
+
+    fn raw_set_permset(entry: acl_entry_t, perm: u32) {
+        unsafe {
+            let mut permset: acl_permset_t = null_mut();
+            check_return(acl_get_permset(entry, &mut permset), "acl_get_permset");
+            check_return(acl_clear_perms(permset), "acl_clear_perms");
+            check_return(acl_add_perm(permset, perm), "acl_add_perm");
+            check_return(acl_set_permset(entry, permset), "acl_set_permset");
+        }
+    }
+
+    fn raw_get_entry(&self, qual: &Qualifier) -> Option<acl_entry_t> {
+        for entry in unsafe { self.raw_iter() } {
+            // XXX this is slightly inefficient, calls to get_entry_uid() could be short-circuited.
+            if Qualifier::from_entry(entry) == *qual {
+                // Found it!
+                return Some(entry);
+            }
+        }
+        None
+    }
+
+    fn raw_add_entry(&mut self, qual: &Qualifier) -> acl_entry_t {
+        let mut entry: acl_entry_t = null_mut();
+        unsafe {
+            check_return(
+                acl_create_entry(&mut self.acl, &mut entry),
+                "acl_create_entry",
+            );
+            check_return(acl_set_tag_type(entry, qual.tag_type()), "acl_set_tag_type");
+            if let Some(uid) = qual.uid() {
+                check_return(
+                    acl_set_qualifier(entry, &uid as *const u32 as *const c_void),
+                    "acl_set_qualifier",
+                );
+            }
+        }
+        entry
+    }
+
+    /// Re-calculate the `Qualifier::Mask` entry.
+    ///
+    /// Usually there is no need to call this directly, as this is done during
+    /// `write_acl/write_default_acl()` automatically.
+    pub fn fix_mask(&mut self) {
+        unsafe {
+            check_return(acl_calc_mask(&mut self.acl), "acl_calc_mask");
+        }
+    }
+
+    /// Return the textual representation of the ACL. Individual entries are separated by newline
+    /// (`'\n'`).
+    ///
+    /// UID/GID are automatically resolved to names by the platform.
+    pub fn as_text(&self) -> String {
+        let mut len: ssize_t = 0;
+        let txt = AutoPtr(unsafe { acl_to_text(self.acl, &mut len) });
+        check_pointer(txt.0, "acl_to_text");
+        let chars = unsafe { from_raw_parts(txt.0 as *const u8, len as usize) };
+
+        from_utf8(chars).unwrap().to_string()
+    }
+
+    fn compact_text(&self) -> String {
+        self.as_text().trim_end().replace('\n', ",")
+    }
+
+    /// Call the platform's validation function. Unfortunately it is not possible to provide
+    /// detailed error messages.
+    pub fn validate(&self) -> Result<(), SimpleError> {
+        let ret = unsafe { acl_valid(self.acl) };
+        if ret != 0 {
+            bail!("Invalid ACL: {}", self.compact_text());
+        }
+        Ok(())
+    }
+
+    /// Consumes the `PosixACL`, returning the wrapped `acl_t`.
+    /// This can then be used directly in FFI calls to the acl library.
+    ///
+    /// To avoid a memory leak, the `acl_t` must either:
+    ///
+    /// - Be converted back to a PosixACL using `PosixACL::from_raw`
+    /// - Have `acl_free` called on it
+    // Note: it's typically considered safe for Rust functions to leak resources (in this specific
+    // case, the function is analogous to the safe `Rc::into_raw` function in the standard library).
+    // For more discussion on this, see [the nomicon](https://doc.rust-lang.org/nomicon/leaking.html).
+    pub fn into_raw(self) -> acl_t {
+        let acl = self.acl;
+        mem::forget(self);
+        acl
+    }
+
+    /// Constructs a `PosixACL` from a raw `acl_t`. You should treat the `acl_t`
+    /// as being 'consumed' by this function.
+    ///
+    /// # Safety
+    ///
+    /// The `acl_t` must be a valid acl (not `(acl_t)NULL`) acl returned
+    /// either `PosixACL::into_raw` or another acl library function.
+    ///
+    /// Improper usage of this function may lead to memory problems (e.g.
+    /// calling it twice on the same acl may lead to a double free).
+    pub unsafe fn from_raw(acl: acl_t) -> Self {
+        Self { acl }
+    }
+}
---
+++ src/entry.rs
@@ -0,0 +1,97 @@
+use std::ptr::null_mut;
+
+use acl_sys::{
+    acl_entry_t, acl_get_permset, acl_get_qualifier, acl_get_tag_type, acl_permset_t, ACL_GROUP,
+    ACL_GROUP_OBJ, ACL_MASK, ACL_OTHER, ACL_UNDEFINED_TAG, ACL_USER, ACL_USER_OBJ,
+};
+
+use crate::util::{check_pointer, check_return, AutoPtr};
+use crate::Qualifier::*;
+
+/// The subject of a permission grant.
+#[derive(Debug, PartialEq)]
+pub enum Qualifier {
+    /// Unrecognized/corrupt entries
+    Undefined,
+    /// Permissions for owner of the file
+    UserObj,
+    /// Permissions for owning group of the file
+    GroupObj,
+    /// Permissions for everyone else not covered by the ACL
+    Other,
+    /// Permissions for user with UID `u32` value
+    User(u32),
+    /// Permissions for group with GID `u32` value
+    Group(u32),
+    /// Auto-generated entry
+    Mask,
+}
+
+impl Qualifier {
+    pub(crate) fn tag_type(&self) -> i32 {
+        match self {
+            Undefined => ACL_UNDEFINED_TAG,
+            UserObj => ACL_USER_OBJ,
+            GroupObj => ACL_GROUP_OBJ,
+            User(_) => ACL_USER,
+            Group(_) => ACL_GROUP,
+            Mask => ACL_MASK,
+            Other => ACL_OTHER,
+        }
+    }
+    pub(crate) fn uid(&self) -> Option<u32> {
+        match self {
+            User(uid) | Group(uid) => Some(*uid),
+            _ => None,
+        }
+    }
+    /// Convert C type acl_entry_t to Rust Qualifier
+    pub(crate) fn from_entry(entry: acl_entry_t) -> Qualifier {
+        let tag_type = 0;
+        let ret = unsafe { acl_get_tag_type(entry, &tag_type) };
+        check_return(ret, "acl_get_tag_type");
+        match tag_type {
+            ACL_UNDEFINED_TAG => Undefined,
+            ACL_USER_OBJ => UserObj,
+            ACL_GROUP_OBJ => GroupObj,
+            ACL_USER => User(Qualifier::get_entry_uid(entry)),
+            ACL_GROUP => Group(Qualifier::get_entry_uid(entry)),
+            ACL_MASK => Mask,
+            ACL_OTHER => Other,
+            _ => {
+                panic!("Unexpected tag type {}", tag_type);
+            }
+        }
+    }
+    /// Helper function for from_entry()
+    fn get_entry_uid(entry: acl_entry_t) -> u32 {
+        unsafe {
+            let uid = AutoPtr(acl_get_qualifier(entry) as *mut u32);
+            check_pointer(uid.0, "acl_get_qualifier");
+            *uid.0
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct ACLEntry {
+    pub qual: Qualifier,
+    pub perm: u32,
+}
+
+impl ACLEntry {
+    /// Convert C type acl_entry_t to Rust ACLEntry
+    pub(crate) fn from_entry(entry: acl_entry_t) -> ACLEntry {
+        let perm;
+        let mut permset: acl_permset_t = null_mut();
+        unsafe {
+            let ret = acl_get_permset(entry, &mut permset);
+            check_return(ret, "acl_get_permset");
+            perm = *(permset as *const u32);
+        }
+        ACLEntry {
+            qual: Qualifier::from_entry(entry),
+            perm,
+        }
+    }
+}
